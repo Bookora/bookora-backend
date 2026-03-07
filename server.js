eaf7b91c600fc0f4692c36385a960372f2755f22
Bookora <landape31@gmail.com>
2026-03-07 21:40:25 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
+const express = require("express");
+const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Bookora AI backend running");
+});
+
+app.post("/incoming-call", (req, res) => {
+  res.send(`
+<Response>
+  <Say>Hello, this is Bookora AI receptionist.</Say>
+</Response>
+`);
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
